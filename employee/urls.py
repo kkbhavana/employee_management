bf00b618573ac155e0382a3b964cd51fddc3c050
kkbhavana <kkbhavana6@gmail.com>
2023-07-31 21:51:22 +0530

    emp

--- employee/urls.py
+++ employee/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('management.urls')),
     path('api-auth/',include('rest_framework.urls')),
+    path('user/',include('account.urls')),
 
 ]
